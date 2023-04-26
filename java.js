5b9ec23bfd2049da2731672286d46b8d52724232
Luis <perezluis167@gmail.com>
2023-04-26 17:51:51 -0400

    update java

--- java.js
+++ java.js
@@ -11,35 +11,28 @@ class Clock {
 
 
   constructor() {
-      // const now = new Date(1980, 6, 31); 
       const now = new Date();
       this.hours = now.getHours()
       this.minutes = now.getMinutes()
       this.seconds = now.getSeconds()
-      setInterval(() => {
-
-        this._tick(); 
-      }, 500);
+      setInterval(() => this._tick(), 1000);
   };
 
   printTime() {
-    // const now = new Date();
     console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
   };
 
   _tick() {
-      setInterval(() => {
         this.seconds ++
         if (this.seconds === 60) {
             this.minutes ++
             this.seconds = 0 
-                if (this.minutes === 60)  {
+                if (this.minutes === 60) {
                        this.hours ++ 
                        this.minutes = 0 
                 };
         };
         this.printTime();
-      }, 1000);
     };
 
   };
